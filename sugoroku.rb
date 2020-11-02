7b34136b3237833a78d578d3914e7abbeafb811e
上田　健太 <kosian.4141@gmail.com>
2020-11-02 11:49:03 +0900

    sugoroku

--- sugoroku.rb
+++ sugoroku.rb
@@ -1,1 +1,23 @@
-初commit
+class Sugoroku
+# マスは20マス
+masu = 20
+# 初期位置
+position = 0
+
+puts "スゴロクゲーム！"
+puts "全部で#{masu}マス"
+
+while position < masu
+	puts "サイコロを振る"
+	gets
+	dice = rand(1..6)
+	puts "サイコロの目は#{dice}です"
+	puts "現在の位置は#{position += dice}マス目です"
+end
+   	puts "ゴール！"
+end
+
+
+
+
+
